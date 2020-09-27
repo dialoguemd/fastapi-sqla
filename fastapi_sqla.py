1b890b6d5962dc643c37ee291a9330870c808195
Hadrien David <hadrien.david@dialogue.co>
2020-09-27 10:22:56 -0400

    comment

--- fastapi_sqla.py
+++ fastapi_sqla.py
@@ -15,17 +15,6 @@ _Session = sessionmaker()
 
 @router.on_event("startup")
 def startup():
-    """Setup sqlalchemy.
-
-    Create the sqla engine instance using environment variables.
-    The keys of interest in `os.environ` are prefixed with `sqlalchemy_`.
-    Each matching key (after the prefix is stripped) is treated as though it were the
-    corresponding keyword argument to [`sqlalchemy.create_engine`]
-    (https://docs.sqlalchemy.org/en/13/core/engines.html?highlight=create_engine#sqlalchemy.create_engine)  # noqa
-    call.
-
-    The only required key is `sqlalchemy_url`, which provides the database URL.
-    """
     engine = engine_from_config(os.environ, prefix="sqlalchemy_")
     _Session.configure(bind=engine)
     logger.info("startup", engine=engine)
