4f2acc87de92c3b72065500e73f90b8b279d8655
Hadrien David <hadrien.david@dialogue.co>
2021-04-05 11:32:51 -0400

    fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -59,5 +59,5 @@ def check_sqlalchemy_version(request):
         current = f"{major}.{minor}"
         if expected != current:
             skip(
-                f"Skipping as it runs against sqlalchemy=^{expected}.0, got {__version__}"
+                f"Marked to run against sqlalchemy=^{expected}.0, but got {__version__}"
             )
