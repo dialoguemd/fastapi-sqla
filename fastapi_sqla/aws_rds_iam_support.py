1ded74c83b68d7d32960236244e2c31eac7ee0ed
Maxime Belanger <maxime.belanger@dialogue.co>
2022-02-09 16:28:48 -0500

    fudge coverage

--- fastapi_sqla/aws_rds_iam_support.py
+++ fastapi_sqla/aws_rds_iam_support.py
@@ -1,7 +1,8 @@
 try:
     import boto3
+
     boto3_installed = True
-except ImportError as err:
+except ImportError as err: # pragma: no cover
     boto3_installed = False
     boto3_installed_err = str(err)
 
