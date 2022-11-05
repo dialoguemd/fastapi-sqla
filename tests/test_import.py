fa42198f2326d06bf89601b71c3feec7f51ead15
Hadrien David <hadrien.david@dialogue.co>
2022-11-05 19:55:02 -0400

    fix: missing Collection & Item - DIA-46202

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,11 +1,25 @@
-from pytest import mark
+from pytest import mark, raises
 
 
-def test_import_fastapi_sqla():
-    import fastapi_sqla  # noqa
+def test_import_sync_api():
+    from fastapi_sqla import (  # noqa
+        Base,
+        Collection,
+        Item,
+        Page,
+        Paginate,
+        Session,
+        open_session,
+        setup,
+    )
 
 
 @mark.require_asyncpg
 @mark.sqlalchemy("1.4")
-def test_import_async_session():
-    from fastapi_sqla import AsyncSession  # noqa
+def test_import_async_api():
+    from fastapi_sqla import (  # noqa
+        AsyncSession,
+        AsyncPagination,
+        AsyncPaginate,
+        open_async_session,
+    )
