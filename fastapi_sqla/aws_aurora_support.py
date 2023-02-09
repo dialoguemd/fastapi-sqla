d3a1344ebafb8194202bd5931999e70e7ff61359
Arthur Loiselle <arthur.loiselle@dialogue.co>
2023-02-09 15:02:08 -0500

    feat(aurora): invalidate connection on readonly transaction error - DIA-51197

---
+++ fastapi_sqla/aws_aurora_support.py
@@ -0,0 +1,31 @@
+from pydantic import BaseSettings
+from sqlalchemy import ExceptionContext, event
+from sqlalchemy.engine import Engine
+
+# Taken from https://www.postgresql.org/docs/current/errcodes-appendix.html#ERRCODES-TABLE
+READONLY_ERROR_CODE = "25006"
+
+
+def setup(engine: Engine):
+    config = Config()
+
+    if not config.aws_aurora_enabled:
+        return
+
+    event.listen(engine, "handle_error", disconnect_on_readonly_error)
+
+
+def disconnect_on_readonly_error(context: ExceptionContext):
+    if context.is_disconnect:
+        return
+
+    error_code = getattr(context.original_exception, "pgcode", None)
+    if error_code == READONLY_ERROR_CODE:
+        context.is_disconnect = True
+
+
+class Config(BaseSettings):
+    aws_aurora_enabled: bool = False
+
+    class Config:
+        env_prefix = "fastapi_sqla_"
---
+++ tests/test_aws_aurora_support.py
@@ -0,0 +1,55 @@
+from unittest.mock import Mock
+
+from pytest import mark, raises
+from sqlalchemy import text
+
+
+@mark.dont_patch_engines
+def test_sync_disconnects_on_readonly_error(monkeypatch):
+    from fastapi_sqla.sqla import _Session, startup
+
+    monkeypatch.setenv("fastapi_sqla_aws_aurora_enabled", "true")
+
+    startup()
+
+    connection = _Session().connection(execution_options={"postgresql_readonly": True})
+    with raises(Exception):
+        connection.execute(text("CREATE TABLE fail(id integer)"))
+
+    assert connection.invalidated
+
+
+@mark.require_asyncpg
+@mark.dont_patch_engines
+async def test_async_disconnects_on_readonly_error(monkeypatch, async_sqlalchemy_url):
+    from fastapi_sqla.asyncio_support import _AsyncSession, startup
+
+    monkeypatch.setenv("fastapi_sqla_aws_aurora_enabled", "true")
+    monkeypatch.setenv("async_sqlalchemy_url", async_sqlalchemy_url)
+
+    await startup()
+
+    connection = await _AsyncSession().connection(
+        execution_options={"postgresql_readonly": True}
+    )
+    with raises(Exception):
+        await connection.execute(text("CREATE TABLE fail(id integer)"))
+
+    assert connection.invalidated
+
+
+@mark.parametrize(
+    "pgcode, should_disconnect", [("25006", True), ("00000", False), (None, False)]
+)
+def test_readonly_error_codes(pgcode, should_disconnect):
+    from fastapi_sqla.aws_aurora_support import disconnect_on_readonly_error
+
+    exception = Mock()
+    exception.pgcode = pgcode
+    context = Mock()
+    context.original_exception = exception
+    context.is_disconnect = False
+
+    disconnect_on_readonly_error(context)
+
+    assert context.is_disconnect == should_disconnect
