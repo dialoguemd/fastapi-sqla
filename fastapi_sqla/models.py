738ae7a76c08b94a18e88ec8021419b2eb1f6ed5
Edouard Lavery-Plante <edouard.lavery-plante@dialogue.co>
2026-07-27 19:21:33 -0400

    feat!: drop pydantic v1 support (#442)
    Removes pydantic v1 support and tests
    
    BREAKING CHANGE: pydantic v1 is no longer compatible with this library

--- fastapi_sqla/models.py
+++ fastapi_sqla/models.py
@@ -1,15 +1,8 @@
 from typing import Generic, TypeVar
 
 from pydantic import BaseModel, Field
-from pydantic import __version__ as pydantic_version
 
-major, _, _ = (int(v) for v in pydantic_version.split("."))
-is_pydantic2 = major == 2
-if is_pydantic2:
-    GenericModel = BaseModel
-else:
-    from pydantic.generics import GenericModel  # type:ignore
-
+GenericModel = BaseModel
 
 ItemT = TypeVar("ItemT")
 
